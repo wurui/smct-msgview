a985ba591bf5c42c634fbd8e2f298a8d5a93538e
wurui <wurui.wr@gmail.com>
2017-06-30 17:21:02 +0800

    new style

--- asset/index.js
+++ asset/index.js
@@ -1,4 +1,9 @@
 define(['zepto','mustache','oxjs'],function(undef,Mustache,OXJS){
+    var apiHost = '//www.shaomachetie.com';
+    if(document.documentElement.getAttribute('env')=='local') {
+        apiHost = 'http://192.168.1.103:8000'
+    }
+    var _id=OXJS.queryString('_id');
     var timeformat = function (d) {
         if (typeof d != 'object') {
             d = new Date(d);
@@ -8,28 +13,53 @@ define(['zepto','mustache','oxjs'],function(undef,Mustache,OXJS){
         };
         return [d.getFullYear(), prefix0(d.getMonth() + 1), prefix0(d.getDate())].join('-') + ' ' + [d.getHours(), prefix0(d.getMinutes()), prefix0(d.getSeconds())].join(':')
     };
+
+    var echoNotify=function(echo,fn){
+
+        $.getJSON(apiHost+'/carnotify/echonotify?&callback=?',{
+            _id:_id,
+            echo:echo
+        },function(data){
+            fn && fn(data)
+        });
+    };
   return {
     init:function($mod){
-        var _id=OXJS.queryString('_id');
+        var tpl=$('.J_tpl', $mod).html();
+        var operation=$mod.attr('data-op');
+        var getAndRender=function(){
+            if(_id){
 
-        if(_id){
-            var apiHost = '//www.shaomachetie.com';
-            if(document.documentElement.getAttribute('env')=='local') {
-                apiHost = 'http://localhost:8000'
+                $.getJSON(apiHost+'/carnotify/getnotify?_id='+_id+'&callback=?',function(data){
+                    if(data && data.sts) {
+                        data.time = timeformat(data.sts);
+                        data.imgs=data.imgs.split(',')
+                        data.shortcuts= operation && Date.now() - data.sts  < 3600*1000;
+                        $mod.html(Mustache.render(tpl, data))
+                    }else{
+                        $mod.html('<div class="empty">页面已过期</div>');
+                    }
+                });
+
+            }else{
+                $mod.html('<div class="empty">无信息</div>')
+            };
+        };
+        getAndRender();
+
+
+        $mod.on('click',function(e){
+            var tar= e.target,
+                role=tar.getAttribute('data-role');
+            switch (role){
+                case 'msd':
+                    echoNotify('msd',getAndRender)
+                    break
+                case 'sendno':
+                    echoNotify('sendno',getAndRender)
+                    break
             }
-            $.getJSON(apiHost+'/carnotify/getnotify?_id='+_id+'&callback=?',function(data){
-                if(data && data.sts) {
-                    data.time = timeformat(data.sts);
-                    data.imgs=data.imgs.split(',')
-                    $mod.html(Mustache.render($('.J_tpl', $mod).html(), data))
-                }else{
-                    $mod.html('<div class="empty">页面已过期</div>');
-                }
-            })
-
-        }else{
-            $mod.html('<div class="empty">无信息</div>')
-        }
+        })
 
 
 
--- mod.xsl
+++ mod.xsl
@@ -1,27 +1,39 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:oxm="https://www.openxsl.com">
     <xsl:template match="/root" name="wurui.smct-msgview">
+        <xsl:param name="operation"/>
         <!-- className 'J_OXMod' required  -->
-        <div class="J_OXMod oxmod-smct-msgview" ox-mod="smct-msgview">
+        <div class="J_OXMod oxmod-smct-msgview" ox-mod="smct-msgview" data-op="{$operation}">
             <div class="empty">
             加载中...
             </div>
             <script type="text/tpl" class="J_tpl"><![CDATA[
-                <div>
-                    时间:{{time}}
+                <div class="msg-box">
+                    <h3>扫码车贴通知:</h3>
+
+                    您的爱车因<big>{{reason}}</big>,请速至现场处理!
+                    <p class="time">{{time}}</p>
+
                 </div>
-                <div>
-                    理由:{{reason}}
-                </div>
-                <h3>
-                    附图:
-                </h3>
+
                 <div class="imgs">
                     {{#imgs}}
                     <p>
-                        <img src="http://i.oxm1.cc/uploads/{{.}}@!w360"/>
+                        <a href="http://i.oxm1.cc/uploads/{{.}}"><img src="http://i.oxm1.cc/uploads/{{.}}@!w360"/></a>
                     </p>
                     {{/imgs}}
                 </div>
+                <div class="echo">
+                    {{#echo}}
+                    <span class="{{.}}"></span><br/>
+                    {{/echo}}
+                </div>
+                {{#shortcuts}}
+                <div class="shortcuts">
+                    快捷回应:
+                    <button data-role="sendno">发送车主手机号给对方</button>
+                    <button data-role="msd">马上到</button>
+                </div>
+                {{/shortcuts}}
                 ]]>
             </script>
         </div>
